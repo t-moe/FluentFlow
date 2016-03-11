efc184ab78157ba0c2a7232790a84f638c4696e3
Ente <ducksource@duckpond.ch>
2016-03-11 01:19:43 +0100

    basic first matchbox

--- core/matchbox.js
+++ core/matchbox.js
@@ -3,35 +3,22 @@ const Matcher = require('./matcher.js');
 
 'use strict';
 
-const fields = { //fields which will be made available on packet.field.* or lastPacket.field.*
-    "tcp" : ["srcport","dstport"],
-    "udp" : ["srcport","dstport"],
-    "ip" : ["src","dst"],
-    "http" : {}
-};
-
-const starters = {
-    "packet": Fluent.Object.currentObject, //make packet.* an alias for currentObject.*
-    "lastPacket": Fluent.Object.lastObject //make lastPacket.* an alias for lastObject.*
-}
-
 const $ = Fluent.Matcher().starter;
+const m = new Matcher();
 
-//Object.prototype.getName = function() { 
-//   var funcNameRegex = /function (.{1,})\(/;
-//   var results = (funcNameRegex).exec((this).constructor.toString());
-//   return (results && results.length > 1) ? results[1] : "";
-//};
-
-var self = {
+const self = {
     load: function(rulesRaw){
         self.rulesRaw = rulesRaw;
         self.rules = eval(self.rulesRaw);
         const builder = new Matcher.Builder()
         self.rules.forEach(function(r){
-            builder.append(r);
+            builder.append(r.end());
         });
+        m.addRules(builder.rules);
     },
+    match: function(obj){
+        m.matchNext(obj);
+    }
 }
 
 module.exports = self;
