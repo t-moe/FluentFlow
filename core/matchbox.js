ebc837f5f0abbd158779e6e750e66cee08b96805
Ente <ducksource@duckpond.ch>
2016-09-23 15:13:13 +0200

    customization of require.root & builtin

--- core/matchbox.js
+++ core/matchbox.js
@@ -11,14 +11,14 @@ const UglifyJS = require('uglify-js');
 const MATCHBOX_ENV = path.join(__dirname, 'matchboxenv.js');
 const MATCHBOX_ENV_HIDDEN_PROPERTIES = [ 'load', 'setConsole' ];
 const MATCHBOX_ENV_CONSOLE_EMIT_FUNCIONS = [ 'log', 'error' ];
+const MATCHBOX_ENV_BUILTINS = ['path'];
 
 module.exports = function (rulesRaw, vmoptions) {
   vmoptions = vmoptions || {};
   vmoptions.require = vmoptions.require || {};
   vmoptions.require.external = true;
-  vmoptions.require.root = __dirname;
-  vmoptions.require.builtin = (vmoptions.require) ? vmoptions.require.builtin || ['path'] : ['path'];
-  //    builtin: (vmoptions.require) ? vmoptions.require.builtin || ['fs', 'path'] : [],
+  vmoptions.require.root = vmoptions.require.root || __dirname;
+  vmoptions.require.builtin = (vmoptions.require.builtin) ? vmoptions.require.builtin.concat(MATCHBOX_ENV_BUILTINS) : MATCHBOX_ENV_BUILTINS;
   // parse javascript code (check for errors)
   UglifyJS.parse(rulesRaw);
   if (!vmoptions.novm) {
